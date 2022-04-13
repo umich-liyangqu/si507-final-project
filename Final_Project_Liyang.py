9be8e38d24703418d14ad7e7bc4be84ca0f2dfb7
Qu Liyang <liyangqu@umich.edu>
2022-04-12 23:46:19 -0400

    Add caching function

--- Final_Project_Liyang.py
+++ Final_Project_Liyang.py
@@ -6,9 +6,20 @@ import requests
 F1COM_STARTING_POINT = "https://www.formula1.com/en/results.html/"
 F1COM_DRIVER_STARTING_POINT = "/drivers.html"
 
+YEAR_DICT = {}
 
-def retrive_F1com_data():
-    pass
+def retrive_F1com_data(i, YEAR_DICT):
+    year = str(i)
+    if year in YEAR_DICT:
+        return YEAR_DICT(year)
+    else:
+        response = requests.get(F1COM_STARTING_POINT+year+F1COM_DRIVER_STARTING_POINT)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        DRIVER_2022 = soup.find("tbody").find_all("a")
+        print(DRIVER_2022)
+        for driver in DRIVER_2022:
+            driver_name = driver.text.strip()
+            print(driver_name)
 
 class Vertex:
     def __init__(self, name):
@@ -21,15 +32,7 @@ class Vertex:
         return string
 
 def main():
-    response = requests.get(F1COM_STARTING_POINT+"2022"+F1COM_DRIVER_STARTING_POINT)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    DRIVER_2022 = soup.find("tbody").find_all("a")
-    print(DRIVER_2022)
-    subject_options = [i.findAll('option') for i in soup.findAll('select', {"name":"driverRef"})]
-    # print(subject_options[0])
-    for driver in DRIVER_2022:
-        driver_name = driver.text.strip()
-        print(driver_name)
+    pass
 
 if __name__ == "__main__":
     # your control code for Part 4 (interactive search) should go here
